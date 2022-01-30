b451c16d22653ac89b4debf1c250693b392a65e0
Stocker7692 <84149645+Stocker7692@users.noreply.github.com>
2022-01-30 14:17:51 +0530

    this commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const button = document.querySelector('button');
-const h1 = document.querySelector('h1');
-button.addEventListener('click', function (){
-    const newColor = makeRandomColor();
-    document.body.style.backgroundColor = newColor;
-    h1.innerText=newColor;
-})
+// const button = document.querySelector('button');
+// const h1 = document.querySelector('h1');
+// button.addEventListener('click', function (){
+//     const newColor = makeRandomColor();
+//     document.body.style.backgroundColor = newColor;
+//     h1.innerText=newColor;
+// })
 
 
 const makeRandomColor = () => {
@@ -12,6 +12,24 @@ const makeRandomColor = () => {
     const g = Math.floor(Math.random()*256);
     const b = Math.floor(Math.random()*256);
     return `rgb(${r},${g},${b})`;
+
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let button of buttons){
+    button.addEventListener('click', colorize);
+}
+
+const h1s = document.querySelectorAll('h1');
+
+for(let h1 of h1s){
+    h1.addEventListener('click', colorize)
+}
+
+function colorize(){
+    this.style.backgroundColor = makeRandomColor();
+        this.style.color = makeRandomColor();
 
 }
 
